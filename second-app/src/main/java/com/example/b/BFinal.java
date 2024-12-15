5bce5bc6f2442aacc68f7b64f65a4348e70ab46e
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-15 19:54:48 +0530

    added class BFinal with main method and calling "b.m2" & "a.m1" methods

---
+++ second-app/src/main/java/com/example/b/BFinal.java
@@ -0,0 +1,15 @@
+package com.example.b;
+
+import com.example.a.A;
+
+public class BFinal {
+
+    public static void main(String[] args) {
+        B b = new B();
+        b.m2();
+
+        A a = new A();
+        a.m1();
+    }
+
+}
