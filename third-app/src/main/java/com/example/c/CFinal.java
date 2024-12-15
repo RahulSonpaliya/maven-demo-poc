b9258314a35503102e151bf2c63e16307fc45916
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-15 20:06:45 +0530

    added class CFinal with main method and calling "c.m3", "b.m2" & "a.m1" methods

---
+++ third-app/src/main/java/com/example/c/CFinal.java
@@ -0,0 +1,19 @@
+package com.example.c;
+
+import com.example.a.A;
+import com.example.b.B;
+
+public class CFinal {
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.m3();
+
+        B b = new B();
+        b.m2();
+
+        A a = new A();
+        a.m1();
+    }
+
+}
